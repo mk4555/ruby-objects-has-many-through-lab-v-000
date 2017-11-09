0b35e5e5c694ea939c0821e71eb5029eb26d451f
Min Kim <mk4555@nyu.edu>
2017-11-09 19:02:22 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,18 +1,1 @@
-class Patient
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-    @appointments = []
-  end
-
-  def add_appointment(appointment)
-    @appointments << appointment
-  end
-
-  def doctors
-    @appointments.collect do |appt|
-      appt.doctor
-    end
-  end
-end
+\
